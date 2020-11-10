429de204d96c270d6ce2d251018dfb4258c16ba4
Fabián Picón <fpicon@redhat.com>
2020-11-10 13:34:51 -0300

    init

---
+++ src/main/java/cl/clh/poc/integracion/rest/Test.java
@@ -0,0 +1,54 @@
+package cl.clh.poc.integracion.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Test {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Test.class);
+	
+	
+	
+	@GetMapping(value = "/validarRut")
+	public boolean vaildar(@RequestParam String rut){
+		
+		return this.validarRut(rut);
+		
+	}
+	
+
+	
+	public boolean validarRut(String rut) {
+
+	    boolean validacion = false;
+	    try {
+	        rut =  rut.toUpperCase();
+	        rut = rut.replace(".", "");
+	        rut = rut.replace("-", "");
+	        int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+	        char dv = rut.charAt(rut.length() - 1);
+
+	        int m = 0, s = 1;
+	        for (; rutAux != 0; rutAux /= 10) {
+	            s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+	        }
+	        if (dv == (char) (s != 0 ? s + 47 : 75)) {
+	            validacion = true;
+	        }
+
+	    } catch (java.lang.NumberFormatException e) {
+	    } catch (Exception e) {
+	    }
+	    return validacion;
+	}
+	
+	
+	
+	
+}
